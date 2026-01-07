4e3e204a085fe024abc0452199cf9722bad78542
José F. Ruiz <jrz899@inlumine.ual.es>
2026-01-07 21:59:42 +0100

    [iss-3] add tests using all configs

---
+++ src/test/suites/custom-configuration/multiple-configuration/multiple-configuration.ts
@@ -0,0 +1,11 @@
+
+import { Suite } from "../../types";
+import { allConfigs } from "./all-configs";
+// import { allowedDependenciesAndAliases } from "./allowed-dependencies-and-aliases";
+// import { customSourceFolderAndSeverityError } from "./custom-source-folder-and-severity-error";
+
+export const multipleConfiguration: Suite[] = [
+    allConfigs,
+    // allowedDependenciesAndAliases,
+    // customSourceFolderAndSeverityError
+];
--- src/test/suites/scenarios.ts
+++ src/test/suites/scenarios.ts
@@ -2,4 +2,7 @@ import { customConfigSuite } from "./custom-configuration/custom-configuration";
 import { defaultConfigSuite } from "./default-configuration/default-config.suite";
 import { Suite } from "./types";
 
-export const suites: Suite[] = [defaultConfigSuite, ...customConfigSuite];
+export const suites: Suite[] = [
+    defaultConfigSuite, 
+    ...customConfigSuite
+];
