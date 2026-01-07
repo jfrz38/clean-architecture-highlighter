1e0f00f63dff3dbc4a7502dce6bef179824ab7a3
José F. Ruiz <jrz899@inlumine.ual.es>
2026-01-07 22:19:09 +0100

    [iss-3] remove unused tests

--- src/test/suites/custom-configuration/multiple-configuration/multiple-configuration.ts
+++ src/test/suites/custom-configuration/multiple-configuration/multiple-configuration.ts
@@ -1,11 +1,7 @@
 
 import { Suite } from "../../types";
 import { allConfigs } from "./all-configs";
-// import { allowedDependenciesAndAliases } from "./allowed-dependencies-and-aliases";
-// import { customSourceFolderAndSeverityError } from "./custom-source-folder-and-severity-error";
 
 export const multipleConfiguration: Suite[] = [
-    allConfigs,
-    // allowedDependenciesAndAliases,
-    // customSourceFolderAndSeverityError
+    allConfigs
 ];
