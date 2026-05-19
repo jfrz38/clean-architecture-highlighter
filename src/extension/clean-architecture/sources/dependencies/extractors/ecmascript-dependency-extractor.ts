558b2c7ba4f357d9141b9659847d8c0fc9384eff
José F. Ruiz <jrz899@inlumine.ual.es>
2026-05-19 14:59:14 +0200

    [iss-52] create common regex extractor

--- src/extension/clean-architecture/sources/dependencies/extractors/ecmascript-dependency-extractor.ts
+++ src/extension/clean-architecture/sources/dependencies/extractors/ecmascript-dependency-extractor.ts
@@ -1,28 +1,15 @@
-import { TextDocument } from "vscode";
-import { DependencyPosition } from "../dependency-position";
-import { ExtractedDependency } from "../extracted-dependency";
-import { DelimitedDependencyExtractor } from "./delimited-dependency-extractor";
+import { DelimitedDependencyExtractor, RegexDependencyPattern } from "./delimited-dependency-extractor";
 
 export class EcmaScriptDependencyExtractor extends DelimitedDependencyExtractor {
 
     private static readonly IMPORT_REGEX = /import\s+([\s\S]*?)\s+from\s+['"]([^'"]+)['"]/g;
 
     constructor() {
-        super();
-    }
-
-    public extract(document: TextDocument): ExtractedDependency[] {
-        const dependencies: ExtractedDependency[] = [];
-        const text = document.getText();
-        let match: RegExpExecArray | null;
-
-        while ((match = EcmaScriptDependencyExtractor.IMPORT_REGEX.exec(text))) {
-            const startPos = document.positionAt(match.index);
-            const endPos = document.positionAt(match.index + match[0].length);
-            const position = new DependencyPosition(startPos.line, startPos.character, endPos.line, endPos.character);
-            dependencies.push(new ExtractedDependency(this.normalizeDependencyPath(match[2]), position));
-        }
-
-        return dependencies;
+        super([
+            {
+                regex: EcmaScriptDependencyExtractor.IMPORT_REGEX,
+                dependencyPaths: match => [match[2]]
+            }
+        ]);
     }
 }
--- src/extension/clean-architecture/sources/dependencies/extractors/java-dependency-extractor.ts
+++ src/extension/clean-architecture/sources/dependencies/extractors/java-dependency-extractor.ts
@@ -1,28 +1,10 @@
-import { TextDocument } from "vscode";
-import { DependencyPosition } from "../dependency-position";
-import { ExtractedDependency } from "../extracted-dependency";
-import { DelimitedDependencyExtractor } from "./delimited-dependency-extractor";
+import { DelimitedDependencyExtractor, RegexDependencyPattern } from "./delimited-dependency-extractor";
 
 export class JavaDependencyExtractor extends DelimitedDependencyExtractor {
 
     private static readonly IMPORT_REGEX = /^[ \t]*import[ \t]+(?:static[ \t]+)?([A-Za-z_$][\w$]*(?:\.[A-Za-z_$][\w$]*)*(?:\.\*)?)[ \t]*;/gm;
 
     constructor() {
-        super('.', true);
-    }
-
-    public extract(document: TextDocument): ExtractedDependency[] {
-        const dependencies: ExtractedDependency[] = [];
-        const text = document.getText();
-        let match: RegExpExecArray | null;
-
-        while ((match = JavaDependencyExtractor.IMPORT_REGEX.exec(text))) {
-            const startPos = document.positionAt(match.index);
-            const endPos = document.positionAt(match.index + match[0].length);
-            const position = new DependencyPosition(startPos.line, startPos.character, endPos.line, endPos.character);
-            dependencies.push(new ExtractedDependency(this.normalizeDependencyPath(match[1]), position));
-        }
-
-        return dependencies;
+        super([{ regex: JavaDependencyExtractor.IMPORT_REGEX }], '.', true);
     }
 }
