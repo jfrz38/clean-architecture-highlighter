3888c96acc8c52ec8dc03c55caa4c517ec6fa038
José F. Ruiz <jrz899@inlumine.ual.es>
2026-06-13 17:04:26 +0200

    feat: add clean architecture lint to core package (#128)

--- packages/core/eslint.config.mjs
+++ packages/core/eslint.config.mjs
@@ -1,3 +1,17 @@
+import cleanArchitecture from "@jfrz38/eslint-plugin-clean-architecture-highlighter";
 import baseConfig from "../../eslint.config.mjs";
 
-export default baseConfig;
+export default [
+    ...baseConfig,
+    {
+        files: ["src/**/*.ts"],
+        plugins: {
+            "clean-architecture-highlighter": cleanArchitecture,
+        },
+        rules: {
+            "clean-architecture-highlighter/no-layer-violation": ["error", {
+                sourceFolder: "src",
+            }],
+        },
+    },
+];
